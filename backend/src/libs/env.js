b97d4dfad020e47156a4d1d060d6758890414e4b
Festus Charles <preciousjamie001@gmail.com>
2025-10-03 02:44:03 +0100

    unignore

---
+++ backend/src/libs/env.js
@@ -0,0 +1,19 @@
+const config ={
+    PORT: 4080,
+
+    MONGO_URI: 'mongodb+srv://Jmx:uxeou2eb0bu5rlzX@jmxcluster.kvk30fd.mongodb.net/Ibro?retryWrites=true&w=majority&appName=JmxCluster',
+    
+    JWT_SECRET:'TOCtl7G80BAylNIjoLIieZazXJeeIRh4GWF1qziaAJ8=',
+    JWT_EXPIRES_IN:"14d",
+
+
+    SMTP_USER:'preciousjamie001@gmail.com',
+    SMTP_PASSWORD:'gikejpnilwvnqexs',
+    NODE_ENV:"development",
+
+    CLIENT_URL:'http://localhost:5173',
+
+
+}
+
+export default config 
